863f5e449db0268d6eb25f04d5f66fa4c324342f
JoKuebler <jonikuebler@gmail.com>
2019-07-08 22:50:18 +0200

    Added flags and show three on screen

--- MiniGameFlags/ViewController.swift
+++ MiniGameFlags/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonOne: UIButton!
+    @IBOutlet var buttonTwo: UIButton!
+    @IBOutlet var buttonThree: UIButton!
+    
+    var countries = [String]()
+    var scores = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        buttonOne.layer.borderWidth = 1
+        buttonTwo.layer.borderWidth = 1
+        buttonThree.layer.borderWidth = 1
+        askQuestion()
+        
+    }
+    
+    func askQuestion() {
+        buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
+        buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
