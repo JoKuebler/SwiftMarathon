3430484dd8408c22521934c69f3d6c9403cbaeff
JoKuebler <jonikuebler@gmail.com>
2019-07-08 22:06:45 +0200

    Initial Commit

---
+++ MiniGameFlags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniGameFlags
+//
+//  Created by Jonas Kübler on 08.07.19.
+//  Copyright © 2019 Jonas.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
