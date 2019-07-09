954b56b7b00a0a7d9867ab9e738ef6b0434c34ed
JoKuebler <jonikuebler@gmail.com>
2019-07-09 22:03:00 +0200

    Added alert and scoring to game and

--- MiniGameFlags/ViewController.swift
+++ MiniGameFlags/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet var buttonThree: UIButton!
     
     var countries = [String]()
-    var scores = 0
+    var score = 0
+    var correctAnswer = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +31,44 @@ class ViewController: UIViewController {
         
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        // Shuffle array of countries
+        countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        // Set images to buttons
         buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
         buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
         buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var alertTitle: String
+        
+        // If correct button is clicked
+        if sender.tag == correctAnswer {
+            alertTitle = "Correct"
+            score += 1
+        } else {
+            alertTitle = "Wrong"
+            score -= 1
+        }
+        
+        // An object that displays an alert message to the user.
+        let ac = UIAlertController(title: alertTitle, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        // An action that can be taken when the user taps a button in an alert.
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        // Present alert
+        self.present(ac, animated: true)
+    }
+    
 
 }
 
