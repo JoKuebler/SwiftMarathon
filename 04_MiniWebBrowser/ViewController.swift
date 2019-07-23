6609474021eed055539bd9c2646f07c366f3c3dc
JoKuebler <jonikuebler@gmail.com>
2019-07-23 20:20:18 +0200

    Initial Commit

---
+++ 04_MiniWebBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  04_MiniWebBrowser
+//
+//  Created by Jonas Kübler on 23.07.19.
+//  Copyright © 2019 Jonas.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
