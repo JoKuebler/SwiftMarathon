d1f7842fcb9f59e29f7174e04b2a072e42d3fd9c
JoKuebler <jonikuebler@gmail.com>
2019-08-05 17:13:44 +0200

    Initial Commit

---
+++ 06_AutoLayoutAdvanced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  06_AutoLayoutAdvanced
+//
+//  Created by Jonas Kübler on 05.08.19.
+//  Copyright © 2019 Jonas.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
