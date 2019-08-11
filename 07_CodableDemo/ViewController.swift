666766089fa8e961093b3bb78bef750c85255266
JoKuebler <jonikuebler@gmail.com>
2019-08-11 13:09:45 +0200

    Initial Commit

---
+++ 07_CodableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  07_CodableDemo
+//
+//  Created by Jonas Kübler on 11.08.19.
+//  Copyright © 2019 Jonas.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
