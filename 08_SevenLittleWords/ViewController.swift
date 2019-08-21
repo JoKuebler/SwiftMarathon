9271a5d10ba4ccb5a7a9aab0d43c94fbe58d3fe6
JoKuebler <jonikuebler@gmail.com>
2019-08-21 19:17:23 +0200

    Initial Commit

---
+++ 08_SevenLittleWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  08_SevenLittleWords
+//
+//  Created by Jonas Kübler on 21.08.19.
+//  Copyright © 2019 Jonas.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
