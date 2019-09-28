fa0796e2a70189108ae71bc0981daf199a60911e
JoKuebler <jonikuebler@gmail.com>
2019-09-28 19:32:40 +0200

    Day 52: Created layout for project and added image picker

---
+++ 11_ImageEditor/11_ImageEditor/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  11_ImageEditor
+//
+//  Created by Jonas Kübler on 28.09.19.
+//  Copyright © 2019 Jonas Kübler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "ImageFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func sacve(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
+    
+}
+
